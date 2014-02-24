1de0fa24734a7f4fadf4dce37058198697a4575a
Chris Foxley-Evans <cfoxleyevans.compsci@gmail.com>
2014-02-24 17:30:38 +0000

    added the cw files

---
+++ SCC312 - Languages And Compilers/Recursive Descent Recogniser/src/Generate.java
@@ -0,0 +1,9 @@
+/**
+ * Created by chris on 24/02/2014.
+ */
+public class Generate extends AbstractGenerate {
+    @Override
+    public void reportError(Token token, String explanatoryMessage) throws CompilationException {
+
+    }
+}
---
+++ SCC312 - Languages And Compilers/Recursive Descent Recogniser/src/SyntaxAnalyser.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+
+/**
+ * Created by chris on 24/02/2014.
+ */
+public class SyntaxAnalyser extends AbstractSyntaxAnalyser {
+    @Override
+    void _statementPart_() throws IOException, CompilationException {
+
+    }
+
+    @Override
+    void acceptTerminal(int symbol) throws IOException, CompilationException {
+
+    }
+}
